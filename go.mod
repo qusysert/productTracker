a3e67560e2da38c8b77cbde5d3c9ad1de7ba2c87
ivan <isdemchuk@gmail.com>
2022-04-27 10:26:17 +0300

    Downloading and converting .xlsx were implemented

---
+++ go.mod
@@ -0,0 +1,8 @@
+module productTracker
+
+go 1.16
+
+require (
+	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/xuri/excelize/v2 v2.6.0
+)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "productTracker/service"
+
+func main() {
+	service.AddProducts("https://drive.google.com/uc?export=download&id=1DMETpkF1UKgYsZ_KYRomea-4BMKxN4NK", 1333)
+}
